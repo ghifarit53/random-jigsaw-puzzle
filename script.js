c8e193996de2f90c6cfb74eff6ccf9078d9a666e
ghifarit53 <mghifarit53@gmail.com>
2024-10-20 19:01:02 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let random_puzzle_links = [
+    // Frida
+    'https://jigex.com/YCjQ2',
+    'https://jigex.com/HGupv',
+    'https://jigex.com/438Jj',
+    'https://jigex.com/oyZmT',
+    'https://jigex.com/kPv2c',
+    'https://jigex.com/Fu24u',
+    'https://jigex.com/wRTah',
+    'https://jigex.com/w9Byt',
+    'https://jigex.com/KzMDr',
+    'https://jigex.com/QS3zH',
+
+    // Diego
+    'https://jigex.com/YCjQ2',
+    'https://jigex.com/HGupv',
+    'https://jigex.com/438Jj',
+    'https://jigex.com/oyZmT',
+    'https://jigex.com/kPv2c',
+    'https://jigex.com/Fu24u',
+    'https://jigex.com/wRTah',
+    'https://jigex.com/w9Byt',
+    'https://jigex.com/KzMDr',
+    'https://jigex.com/QS3zH',
+];
+
+let puzzle_link = random_puzzle_links[Math.floor(Math.random() * random_puzzle_links.length)];
+
+window.location.href = puzzle_link;
